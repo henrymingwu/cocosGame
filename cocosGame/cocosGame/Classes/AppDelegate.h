0b449efc7843e106c842d5e27022b9ad53ade567
Henry Wu <henry.wu@berkeley.edu>
2014-03-03 00:21:54 -0800

    Initial Commit

---
+++ cocosGame/cocosGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  cocosGame
+//
+//  Created by Henry Wu on 3/3/14.
+//  Copyright HWu 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ cocosGame/cocosGame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cocosGame
+//
+//  Created by Henry Wu on 3/3/14.
+//  Copyright HWu 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
